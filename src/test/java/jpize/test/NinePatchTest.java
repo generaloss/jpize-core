1e48f65792b12bb8dd1fc43989fdee341950066f
generaloss <goloborodkojava@gmail.com>
2024-12-19 22:05:51 +0300

    added NinePatch

--- src/test/java/jpize/test/NinePatchTest.java
+++ src/test/java/jpize/test/NinePatchTest.java
@@ -7,6 +7,7 @@ import jpize.glfw.Glfw;
 import jpize.glfw.init.GlfwPlatform;
 import jpize.util.ninepatch.NinePatch;
 import jpize.util.mesh.TextureBatch;
+import jpize.util.ninepatch.StretchMode;
 
 public class NinePatchTest extends JpizeApplication {
 
@@ -16,15 +17,18 @@ public class NinePatchTest extends JpizeApplication {
     public void init() {
         Gl.clearColor(0.3, 0.6, 0.9);
         this.batch = new TextureBatch();
+
         this.ninePatch = new NinePatch();
-        ninePatch.load("/button.9.png");
+        this.ninePatch.scale().set(1);
+        this.ninePatch.load("/dialog.9.png");
+        this.ninePatch.setStretchMode(StretchMode.STRETCH);
     }
 
     @Override
     public void render() {
         Gl.clearColorBuffer();
         batch.setup();
-        ninePatch.draw(batch, 0, 0, Jpize.getX(), Jpize.getY());
+        ninePatch.draw(batch, 50, 50, Jpize.getX() - 50, Jpize.getY() - 50);
         batch.render();
     }
 
@@ -38,8 +42,7 @@ public class NinePatchTest extends JpizeApplication {
         if(System.getProperty("os.name").equals("Linux"))
             Glfw.glfwInitHintPlatform(GlfwPlatform.X11); // waiting for fixes in lwjgl 3.3.6
 
-        Jpize.create(720, 480, "Window").build().setApp(new NinePatchTest());
-
+        Jpize.create(1280, 720, "Window").build().setApp(new NinePatchTest());
         Jpize.run();
     }
 
