ceab8174ae0d1c45131294d0a53e97d1e0c9abb9
generaloss <generaloss@proton.me>
2025-03-27 08:40:51 +0300

    separate freetype lib

--- jni/freetype/src/jpize_stb_freetype_FreeTypeSize.cpp
+++
@@ -1,7 +0,0 @@
-#include "headers/jpize_stb_freetype_FreeTypeSize.h"
-#include <ft2build.h>
-#include FT_FREETYPE_H
-
-JNIEXPORT jlong JNICALL Java_jpize_stb_freetype_FreeTypeSize_getMetrics (JNIEnv *, jclass, jlong sizePtr) {
-    return reinterpret_cast<jlong>(&reinterpret_cast<FT_Size>(sizePtr)->metrics);
-}
